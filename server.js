a65f9762e9d4295b212837f60d9454fad9384027
Brandon Cadaoas <brandoncadaoas@gmail.com>
2021-03-03 20:33:39 -0500

    mod: Completed User CRUD endpoints with HTTP response tests

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const UserDB = require("./modules/userDB");
+
+const userDB = new UserDB("mongodb+srv://admin:vqiDJEpwuR2wnfvG@application-data.towvq.mongodb.net/user-data?retryWrites=true&w=majority");
 
 const app = express();
 
@@ -11,4 +14,44 @@ app.get("/", (req, res) => {
     res.json({message : "API Listening"});
 });
 
-app.listen(HTTP_PORT);
+/// User endpoints
+
+app.get("/api/users/:id", (req, res) => {
+    userDB.getUserById(req.params.id).then(data => {
+        res.json(data);
+    }).catch(err => {
+        res.status(400).json({message : `Error: ${err.message}`});
+    });
+});
+
+app.post("/api/users", (req, res) => {
+    userDB.addNewUser(req.body).then(msg => {
+        res.status(201).json({message : msg});
+    }).catch(err => {
+        res.status(500).json({message : `Error: ${err.message}`});
+    });
+});
+
+app.put("/api/users/:id", (req, res) => {
+    userDB.updateUserById(req.body, req.params.id).then(msg => {
+        res.status(200).json({message: msg});
+    }).catch(err =>{
+        res.status(500).json({message: `Error: ${err.message}`});
+    });
+});
+
+app.delete("/api/users/:id", (req, res) => {
+    userDB.deleteUserById(req.params.id).then(msg => {
+        res.status(200).json({message: msg});
+    }).catch(err => {
+        res.status(500).json({message: `Error: ${err.message}`});
+    });
+});
+
+userDB.initialize().then(() => {
+    app.listen(HTTP_PORT, () => {
+        console.log(`Server listening on: ${HTTP_PORT}`);
+    });
+}).catch(err => {
+    console.log(err);
+});
